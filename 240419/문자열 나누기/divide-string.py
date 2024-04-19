5692ee74ce6303b75ff5d9d254e85c45778578cc
kyg1225 <daily1225@gmail.com>
2024-04-19 22:04:58 +0900

    Title: 문자열 나누기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/문자열 나누기/divide-string.py
+++ 240419/문자열 나누기/divide-string.py
@@ -6,4 +6,5 @@ s = list(''.join(s))
 for i in range(1, len(s)):
     if i%5 == 0:
         print(''.join(s[i-5:i]))
-print(''.join(s[-(len(s)%5):]))
+if len(s)%5 != 0:
+    print(''.join(s[-(len(s)%5):]))
