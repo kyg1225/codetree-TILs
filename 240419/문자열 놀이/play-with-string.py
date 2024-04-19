46a2377065e64b2c25c784dc7a6cd398f18bf7f1
kyg1225 <daily1225@gmail.com>
2024-04-19 23:34:43 +0900

    Title: 문자열 놀이, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/문자열 놀이/play-with-string.py
+++ 240419/문자열 놀이/play-with-string.py
@@ -2,14 +2,14 @@ s, q = input().split()
 qs = [list(input().split()) for _ in range(int(q))]
 
 for i in qs:
-    tmp = ''
     if i[0] == '1':
         s = list(s)
         tmp = s[int(i[1])-1]
         s[int(i[1])-1] = s[int(i[2])-1]
         s[int(i[2])-1] = tmp
-        print(''.join(s))
+        s = ''.join(s)
+        print(s)
 
     if i[0] == '2':
-        s = ''.join(s)
-        print(s.replace(i[1], i[2]))
+        s = s.replace(i[1], i[2])
+        print(s)
