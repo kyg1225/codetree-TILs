6dce17098698f4d4f3aabeaa12c59b4afbd41104
kyg1225 <daily1225@gmail.com>
2024-04-20 17:03:14 +0900

    Title: 문자 제거하기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/문자 제거하기/del-char.py
+++ 240420/문자 제거하기/del-char.py
@@ -1,11 +1,12 @@
-s = list(input())
+s = input()
 nums = list(int(input()) for _ in range(len(s)-1))
 
-for i in range(len(nums)):
-    if i != len(nums)-1:
-        s.remove(s[nums[i]])
-        print(''.join(s))
+leng = len(s)
+result = list(s)
+for i in nums:
+    if i >= len(result):
+        result.pop()
+        print(''.join(result))
     else:
-        if nums[i] >= len(s):
-            s.remove(s[-1])
-            print(''.join(s))
+        result.pop(i)
+        print(''.join(result))
