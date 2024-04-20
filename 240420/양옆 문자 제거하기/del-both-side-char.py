39f0ab76b9fb85ddae081d3bdff1a9142dd356c0
kyg1225 <daily1225@gmail.com>
2024-04-20 16:25:39 +0900

    Title: 양옆 문자 제거하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/양옆 문자 제거하기/del-both-side-char.py
+++ 240420/양옆 문자 제거하기/del-both-side-char.py
@@ -1,5 +1,7 @@
 s = list(input())
 
-s.remove(s[1])
-s.remove(s[-2])
-print(''.join(s))
+for i in range(len(s)):
+    if i == 1 or i == len(s)-2:
+        pass
+    else:
+        print(s[i],end='')
