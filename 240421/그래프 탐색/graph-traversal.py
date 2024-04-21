8f72bdaef58da989b6c8a9234eafc1470babefca
kyg1225 <daily1225@gmail.com>
2024-04-21 16:16:56 +0900

    Title: 그래프 탐색, Time: 249ms, Memory: 26MB, Status: Passed - CodeTree

--- 240421/그래프 탐색/graph-traversal.py
+++ 240421/그래프 탐색/graph-traversal.py
@@ -1,25 +1,25 @@
-def dfs(idx):
-    global node_cnt, visited
-    visited[idx] = 1
-
-    for next_node in graph[idx]:
-        if not visited[next_node]:
-            visited[next_node] = 1
+def dfs(node):
+    global visited, node_cnt
+    visited[node] = 1
+    for next_n in graph[node]:
+        if not visited[next_n] and graph[next_n]:
+            visited[next_n] = 1
             node_cnt += 1
-            dfs(next_node)
+            dfs(next_n)
 
 n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
 visited = [0 for _ in range(n+1)]
+
 node_cnt = 0
 
 for i in range(m):
-    x, y = map(int, input().split())
+    start_node, end_node = map(int, input().split())
 
-    graph[x].append(y)
-    graph[y].append(x)
+    graph[start_node].append(end_node)
+    graph[end_node].append(start_node)
 
-dfs(1)
-
+root_node = 1
+dfs(root_node)
 print(node_cnt)
