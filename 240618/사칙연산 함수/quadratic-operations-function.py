350b5678c1320e03f0a92fc582432df88021d558
kyg1225 <daily1225@gmail.com>
2024-06-18 01:29:22 +0900

    Title: 사칙연산 함수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240618/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,26 @@
+def add(n1,n2):
+    return n1+n2
+
+def minus(n1,n2):
+    if n1>n2:
+        return n1-n2
+    return n2-n1
+
+def multi(n1, n2):
+    return n1*n2
+
+def div(n1, n2):
+    return n1//n2
+
+
+a,o,c = map(str, input().split())
+if o == '+':
+    print(f'{a} {o} {c} = {add(int(a), int(c))}')
+elif o == '-':
+    print(f'{a} {o} {c} = {minus(int(a), int(c))}')
+elif o == '*':
+    print(f'{a} {o} {c} = {multi(int(a), int(c))}')
+elif o == '/':
+    print(f'{a} {o} {c} = {div(int(a), int(c))}')
+else:
+    False
