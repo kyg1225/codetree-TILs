cd68bf7b308a57cdbf738575da2fc29ede063553
kyg1225 <daily1225@gmail.com>
2024-06-17 17:30:04 +0900

    Title: 정수의 최솟값, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240617/정수의 최솟값/minimum-value-of-an-integer.py
+++ 240617/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,16 +1,11 @@
 def smallest(n1, n2, n3):
-    if n1 == n2 and n1 ==n3:
-        print(n1)
-    else:
-        if n1 < n2:
-            if n1 < n3 or n1 == n3:
-                print(n1)
-        elif n2 < n1:
-            if n2 < n3 or n2 == n3:
-                print(n2)
-        elif n3 < n1:
-            if n3 < n2 or n3 == n2:
-                print(n3)
+    mini = n1
+    if mini > n2:
+        mini = n2
+    if mini > n3:
+        mini = n3
+        
+    print(mini)
     
 
 a, b, c = map(int, input().split())
