6db3817dd0cae2334a5eaeda3f3cd77c1a6a9a0f
kyg1225 <daily1225@gmail.com>
2024-06-17 18:24:36 +0900

    Title: 함수를 이용한 윤년 판별, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240617/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,8 @@
 def is_yun(y):
     if y%4!=0:
         return 'false'
-        if y%100 == 0 and y%400 != 0:
-            return 'false'
+    if y%100 == 0 and y%400 != 0:
+        return 'false'
     return 'true'
 
 y = int(input())
