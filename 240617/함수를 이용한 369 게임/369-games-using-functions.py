739de0c6a879eac51d316ac7d2615b9bf4bb19cc
kyg1225 <daily1225@gmail.com>
2024-06-17 17:43:12 +0900

    Title: 함수를 이용한 369 게임, Time: 239ms, Memory: 37MB, Status: Passed - CodeTree

--- 240617/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240617/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,11 +1,11 @@
 def is_magic_num(n, m):
     num_list = []
     for i in range(n, m+1):
-        if i%3==0 or is_in_three(i)==True:
+        if i%3==0 or in_369(i)==True:
             num_list.append(i)
     return len(num_list)
 
-def is_in_three(num):
+def in_369(num):
     if '3' in str(num) or '6' in str(num) or '9' in str(num):
         return True
 
