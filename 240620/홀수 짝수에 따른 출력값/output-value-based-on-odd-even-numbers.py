93106df9351daca3bf7465144017618b572e2448
kyg1225 <daily1225@gmail.com>
2024-06-20 23:47:46 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 240620/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,11 +1,10 @@
 n = int(input())
 
 def get_sum(n):
-    if n==0:
+    if n==1:
         return 1
-    if n%2==0:
-        return get_sum(n//2)+n
-    else:
-        return get_sum(n//2)+n
+    if n==2:
+        return 2
+    return get_sum(n-2)+n
 
 print(get_sum(n))
