4ca4a7f49849afe99584c9e4abed1c12431a78b0
kyg1225 <daily1225@gmail.com>
2024-06-28 00:03:18 +0900

    Title: 2진수로 변환하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240628/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,12 @@
+n = int(input())
+# que = []
+# while True:
+#     if n < 2:
+#         break
+#     que.insert(0, n%2)
+#     n = n//2
+# que.insert(0, n)
+
+# for i in que:
+#     print(i, end='')
+print(bin(n)[2:])
