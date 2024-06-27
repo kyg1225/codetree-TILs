a1db3515640b833e099449640cf8b7790291891e
kyg1225 <daily1225@gmail.com>
2024-06-27 20:47:42 +0900

    Title: 개인정보, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/개인정보/personal-info.py
+++ 240627/개인정보/personal-info.py
@@ -1,14 +1,16 @@
 personal=[] 
 for i in range(5):
     name, height, kg = tuple(input().split())
-    personal.append((name, int(height), int(kg)))
+    personal.append((name, int(height), float(kg)))
 
-personal.sort(key=lambda x:x[0])
+personal.sort(key=lambda x: x[0])
 print('name')
 for p in personal:
     print(p[0], p[1], p[2])
 
-personal.sort(key=lambda x:x[1])
+print('')
+
+personal.sort(key=lambda x: -x[1])
 print('height')
 for p in personal:
     print(p[0], p[1], p[2])
