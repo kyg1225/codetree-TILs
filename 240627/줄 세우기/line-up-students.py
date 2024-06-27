d69ff9d603b925b1b8ed562af37afaec0f6b2607
kyg1225 <daily1225@gmail.com>
2024-06-27 16:25:52 +0900

    Title: 줄 세우기, Time: 210ms, Memory: 25MB, Status: Passed - CodeTree

--- 240627/줄 세우기/line-up-students.py
+++ 240627/줄 세우기/line-up-students.py
@@ -4,7 +4,7 @@ for i in range(1, n+1):
     h, kg = tuple(map(int, input().split()))
     h_sort.append((h, kg, i))
 
-h_sort.sort(key=lambda x:(-x[0], -x[1], -x[2]))
+h_sort.sort(key=lambda x:(-x[0], -x[1], x[2]))
 
 for h in h_sort:
     print(h[0], h[1], h[2])
