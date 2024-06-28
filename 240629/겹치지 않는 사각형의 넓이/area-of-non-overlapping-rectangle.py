a29a9605d2e956dcd4910aa2f47f3e08424f6866
kyg1225 <daily1225@gmail.com>
2024-06-29 03:11:15 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240629/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 240629/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -8,10 +8,10 @@ for idx, (x1, y1, x2, y2) in enumerate(rects):
     x1, y1 = x1 + OFFSET, y1 + OFFSET
     x2, y2 = x2 + OFFSET, y2 + OFFSET
     
-    if idx == 1:
+    if idx == 2:
         for x in range(x1, x2):
             for y in range(y1, y2):
-                checked[x][y] = -1
+                checked[x][y] = 0
     else:
         for x in range(x1, x2):
             for y in range(y1, y2):
