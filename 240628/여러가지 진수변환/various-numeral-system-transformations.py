352f5397bfae8c4e515d4ebd55c9996922d7e9f4
kyg1225 <daily1225@gmail.com>
2024-06-28 14:47:56 +0900

    Title: 여러가지 진수변환, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240628/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,12 @@
+n, b = map(int, input().split())
+
+result = []
+while True:
+    if n<b:
+        result.append(n)
+        break
+    result.append(n%b)
+    n//=b
+
+for i in result[::-1]:
+    print(i, end='')
