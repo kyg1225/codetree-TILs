4fa75aa47da72beed3bfcc3e27375df96c82e534
kyg1225 <daily1225@gmail.com>
2024-06-28 23:55:00 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240628/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240628/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,14 +1,20 @@
+MAX_R = 2000
+
 n = int(input())
 moving = [tuple(input().split()) for _ in range(n)]
-checked = [0]*21
+checked = [0]*(MAX_R+1)
 
-for x, p in moving:
-    if p=='R':
-        for i in range(int(x), int(x)+int(x)):
+cur = 1000
+for x, d in moving:
+    if d=='R':
+        for i in range(int(x)):
             checked[i]+=1
+        cur += int(x)
     else:
-        for i in range(int(x), 0,-1):
+        for i in range(int(x)):
             checked[i]+=1
+        cur -= int(x)
+
 cnt = 0
 for i in checked:
     if i>=2:
