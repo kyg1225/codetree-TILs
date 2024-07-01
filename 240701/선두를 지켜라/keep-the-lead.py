5b02a842fcc28d554c410399fc253aa3a13514c1
kyg1225 <daily1225@gmail.com>
2024-07-01 21:51:33 +0900

    Title: 선두를 지켜라, Time: 175ms, Memory: 40MB, Status: Passed - CodeTree

--- 240701/선두를 지켜라/keep-the-lead.py
+++ 240701/선두를 지켜라/keep-the-lead.py
@@ -3,29 +3,30 @@ n, m = map(int, input().split())
 
 a_vt, b_vt = [0]*(MAX_T+1), [0]*(MAX_T+1)
 
-time_a = 0
+time_a = 1
 for _ in range(n):
     v, t = map(int, input().split())
     for _ in range(t):
-        a_vt[time_a] += v
+        a_vt[time_a] = a_vt[time_a - 1] + v
         time_a += 1
 
-time_b = 0
+time_b = 1
 for _ in range(m):
     v, t = map(int, input().split())
     for _ in range(t):
-        b_vt[time_b] += v
+        b_vt[time_b] = b_vt[time_b - 1] + v
         time_b += 1
 
 wins = []
 cnt = 0
-for i in range(time_a):
+for i in range(1, time_a):
     if a_vt[i] > b_vt[i]:
-        wins.append('A')
+        if wins == 2:
+            cnt += 1
+        wins = 1
     elif a_vt[i] < b_vt[i]:
-        wins.append('B')
+        if wins == 1:
+            cnt += 1
+        wins = 2
 
-for i in range(1, len(wins)):
-    if wins[i] != wins[i-1]:
-        cnt+=1
 print(cnt)
