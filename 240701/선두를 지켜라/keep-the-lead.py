ad4229043612a5f9e709730a0d968b2d86a4ff6e
kyg1225 <daily1225@gmail.com>
2024-07-01 21:40:37 +0900

    Title: 선두를 지켜라, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240701/선두를 지켜라/keep-the-lead.py
+++ 240701/선두를 지켜라/keep-the-lead.py
@@ -1,4 +1,4 @@
-MAX_T = 1000000
+MAX_T = 20
 n, m = map(int, input().split())
 
 a_vt, b_vt = [0]*(MAX_T+1), [0]*(MAX_T+1)
@@ -17,8 +17,15 @@ for _ in range(m):
         b_vt[time_b] += v
         time_b += 1
 
-ans = 0
-for i in range(1, time_b):
+wins = 'A'
+cnt = 0
+for i in range(time_a):
     if a_vt[i] > b_vt[i]:
-        ans += 1
-print(ans)
+        if wins != 'A':
+            wins = 'A'
+            cnt += 1
+    elif a_vt[i] < b_vt[i]:
+        if wins != 'B':
+            wins = 'B'
+            cnt += 1
+print(cnt)
