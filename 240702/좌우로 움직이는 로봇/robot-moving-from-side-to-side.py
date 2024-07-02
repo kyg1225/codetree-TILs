d8d7286f70280d643316524fa76474f3014259ec
kyg1225 <daily1225@gmail.com>
2024-07-02 15:52:04 +0900

    Title: 좌우로 움직이는 로봇, Time: 122ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240702/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240702/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,4 +1,4 @@
-MAX_T = 20
+MAX_T = 1000000
 
 n, m = map(int, input().split())
 pos_a, pos_b = [0]*(MAX_T+1), [0]*(MAX_T+1)
@@ -6,19 +6,19 @@ pos_a, pos_b = [0]*(MAX_T+1), [0]*(MAX_T+1)
 time_a = 1
 for _ in range(n):
     t, d = tuple(input().split())
-    for _ in range(int(t)):
+    for _ in range(int(t)+1):
         pos_a[time_a] = pos_a[time_a-1] + (1 if d=='R' else -1)
         time_a += 1
     
 time_b = 1
 for _ in range(m):
     t, d = tuple(input().split())
-    for _ in range(int(t)):
+    for _ in range(int(t)+1):
         pos_b[time_b] = pos_b[time_b-1] + (1 if d=='R' else -1)
         time_b += 1
 
 cnt = 0
-for i in range(1, len(pos_a)):
+for i in range(1, time_a+10):
     if pos_a[i] == pos_b[i] and pos_a[i-1] != pos_b[i-1]:
         cnt+=1
 
