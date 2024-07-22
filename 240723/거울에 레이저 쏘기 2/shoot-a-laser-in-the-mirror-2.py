5a79ed0bed4bcb7781eca36401a89a500f570ca5
ygkim <daily1225@gmail.com>
2024-07-23 02:06:12 +0900

    Title: 거울에 레이저 쏘기 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240723/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,87 +1,35 @@
 n = int(input())
-maps = [list(input()) for _ in range(n)]
+arr = [input() for _ in range(n)]
 k = int(input())
+
+def initialize(num):
+    if num<=n:
+        return 0, num-1, 0
+    elif num <= 2 * n:
+        return num - n - 1, n - 1, 1
+    elif num <= 3 * n:
+        return n - 1, n - (num - 2 * n), 2
+    else:
+        return n - (num - 3 * n), 0, 3
 
 def in_range(x, y):
     return 0<=x and x<n and 0<=y and y<n
 
+def move(x, y, next_dir):
+    dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+    nx, ny = x+dxs[next_dir], y+dys[next_dir]
+    return nx, ny, next_dir
 
-def nums_123(x, y):
-    dxs, dys = [0,-1, 1, 0], [1, 0, 0, -1]
-    dir_num = 2
-    result = 1
+def simulate(x, y, move_dir):
+    move_num = 0
     while in_range(x, y):
-        if maps[x][y]=='\\':
-            dir_num = (dir_num+3)%4
-            x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
+        if arr[x][y] == '/':
+            x, y, move_dir = move(x, y, move_dir^1)
         else:
-            dir_num = (dir_num+1)%4
-            x, y = x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-    return result+1
+            x, y, move_dir = move(x, y, 3-move_dir)
+        move_num+=1
+    return move_num
 
-def nums_456(x, y):
-    dxs, dys = [0,-1, 1, 0], [1, 0, 0, -1]
-    dir_num = 3
-    result = 1
-    while in_range(x, y):
-        if maps[x][y]=='\\':
-            dir_num = (dir_num+1)%4
-            x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-        else:
-            dir_num = (dir_num+3)%4
-            x, y = x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-    return result+1
-
-def nums_789(x, y):
-    dxs, dys = [0,-1, 1, 0], [1, 0, 0, -1]
-    dir_num = 1
-    result = 1
-    while in_range(x, y):
-        if maps[x][y]=='\\':
-            dir_num = (dir_num+3)%4
-            x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-        else:
-            dir_num = (dir_num+1)%4
-            x, y = x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-    return result+1
-
-def nums_101112(x, y):
-    dxs, dys = [0,-1, 1, 0], [1, 0, 0, -1]
-    dir_num = 0
-    result = 1
-    while in_range(x, y):
-        if maps[x][y]=='\\':
-            dir_num = (dir_num+1)%4
-            x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-        else:
-            dir_num = (dir_num+3)%4
-            x, y = x, y = x+dxs[dir_num], y+dys[dir_num]
-            result += 1
-    return result+1
-
-if 1<= k < 4:
-    dir_num = 2
-    x, y = 0, k-1
-    print(nums_123(x, y))
-    
-elif k < 7:
-    dir_num = 3
-    x, y = k-4, 2
-    print(nums_456(x, y))
-
-elif k < 10:
-    dir_num = 0
-    x, y = abs(k-9), 0
-    print(nums_789(x, y))
-
-else:
-    dir_num = 1
-    x, y = 2, abs(k-12)
-    print(nums_101112(x, y))
+x, y, move_dir = initialize(k)
+move_num = simulate(x, y, move_dir)
+print(move_num)
