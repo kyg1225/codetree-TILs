2c9bd47321be8518b302e2db54f3a25fb022c6f1
ygkim <daily1225@gmail.com>
2024-07-22 22:22:56 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+x, y, d = 0, 0, 0
+
+maps = [[0]*m for _ in range(n)]
+maps[x][y] = 1
+
+def in_range(x, y):
+    return 0<=x and x<n and 0<=y and y<n
+
+for i in range(2, n*m + 1):
+    nx, ny = x + dxs[d], y + dys[d]
+    if not in_range(nx, ny) or maps[nx][ny] !=0:
+        d = (d + 1)%4
+    x, y = x + dxs[d], y + dys[d]
+    maps[x][y] = i
+
+for i in range(n):
+    for j in range(m):
+        print(maps[i][j], end=' ')
+    print()
