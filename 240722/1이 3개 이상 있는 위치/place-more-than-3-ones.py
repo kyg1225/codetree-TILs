30e38b150976683476c36b82824bb847ae8e9b3b
ygkim <daily1225@gmail.com>
2024-07-22 20:25:16 +0900

    Title: 1이 3개 이상 있는 위치, Time: 160ms, Memory: 26MB, Status: Passed - CodeTree

--- 240722/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240722/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -7,14 +7,18 @@ dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
 def in_range(x, y):
     return 0<=x and x<n and 0<=y and y<n
 
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and maps[nx][ny]==1:
+            cnt+=1
+    return cnt
+
 result = 0
 for i in range(n):
     for j in range(n):
-        cnt=0
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x+i+dx, y+j+dx
-            if in_range(nx, ny) and maps[nx][ny]==1:
-                cnt+=1
-        if cnt>=3:
+        if adjacent_cnt(i, j) >= 3:
             result+=1
-print(result-1)
+            
+print(result)
