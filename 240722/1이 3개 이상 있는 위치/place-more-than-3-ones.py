111c33258ae8c1655e4c11644a183561366c0fee
ygkim <daily1225@gmail.com>
2024-07-22 20:21:26 +0900

    Title: 1이 3개 이상 있는 위치, Time: 125ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240722/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240722/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,25 +1,20 @@
 n = int(input())
 maps = [list(map(int, input().split())) for _ in range(n)]
 
-x = [i for i in range(1, n)]
-y = [i for i in range(1, n)]
+x, y = 0, 0
 dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
 
 def in_range(x, y):
     return 0<=x and x<n and 0<=y and y<n
 
-
-cnt, result = 0, 0
-for i in range(n-1):
-    for j in range(n-1):
-        if cnt >= 3:
-            result += 1
-            cnt=0
-        else:
-            cnt=0
+result = 0
+for i in range(n):
+    for j in range(n):
+        cnt=0
         for dx, dy in zip(dxs, dys):
-            nx, ny = x[i]+dx, y[j]+dx
-            # print(nx, ny)
+            nx, ny = x+i+dx, y+j+dx
             if in_range(nx, ny) and maps[nx][ny]==1:
                 cnt+=1
-print(result)
+        if cnt>=3:
+            result+=1
+print(result-1)
