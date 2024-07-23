0a5a3913f4e9c945aa0faa0aa2f637c0e9c482cd
ygkim <daily1225@gmail.com>
2024-07-24 00:27:15 +0900

    Title: 체크판위에서 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/체크판위에서 2/on-the-checkboard-2.py
+++ 240724/체크판위에서 2/on-the-checkboard-2.py
@@ -1,5 +1,5 @@
-r, c = map(int, input().split())
-arr = [list(input()) for _ in range(r)]
+r, c = tuple(map(int, input().split()))
+arr = [input().split() for _ in range(r)]
 
 start = arr[0][0]
 end = arr[r-1][c-1]
@@ -9,8 +9,8 @@ for i in range(1, r):
     for j in range(1, c):
         for k in range(i+1, r-1):
             for l in range(j+1, c-1):
-                if start != arr[i][j] and arr[i][j]!=arr[k][l] and \
-                arr[k][l] != end:
-                    cnt+=1
-
+                if start != arr[i][j] and \
+                   arr[i][j] != arr[k][l] and \
+                   arr[k][l] != end:
+                   cnt+=1
 print(cnt)
