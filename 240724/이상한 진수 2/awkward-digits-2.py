4c6526527c55c185ee7c793c7b443f45a97fea73
ygkim <daily1225@gmail.com>
2024-07-24 01:02:24 +0900

    Title: 이상한 진수 2, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/이상한 진수 2/awkward-digits-2.py
+++ 240724/이상한 진수 2/awkward-digits-2.py
@@ -1,4 +1,5 @@
 a = list(input())
+# a = [int(i) for i in a]
 
 max_num, num = 0, 0
 cnt = 0
@@ -8,9 +9,9 @@ for i in range(len(a)):
         a[i] = '1'
         cnt += 1
     elif '0' not in a and cnt==0:
-        a[len(a)-1]=0
+        a[len(a)-1]='0'
         cnt+=1
-    num += int(a[len(a)-i-1])*2**i
+    num = int(''.join(a), 2)
     max_num = max(max_num, num)
 
 print(max_num)
