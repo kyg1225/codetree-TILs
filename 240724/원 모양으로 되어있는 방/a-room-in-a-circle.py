7549ab0db06c728a6c074bf47c1fe5b17ca3b32e
ygkim <daily1225@gmail.com>
2024-07-24 22:21:26 +0900

    Title: 원 모양으로 되어있는 방, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,17 @@
+import sys
+INT_MAX = sys.maxsize
+
+
+n = int(input())
+people = [int(input()) for _ in range(n)]
+
+min_dist = INT_MAX
+for i in range(n):
+    sum_dist = 0
+    for j in range(n):
+        dist = (j+n-i)%n
+        sum_dist += dist * arr[j]
+
+    min_dist = min(min_dist, sum_dist)
+
+print(min_dist)
