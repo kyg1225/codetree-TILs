852a7704073bc5d931dd108d9b119f3482174398
ygkim <daily1225@gmail.com>
2024-07-24 17:27:29 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240724/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -9,7 +9,7 @@ for i in range(1, n-1):
     dist = 0
     prev_idx = 0
     for j in range(1, n):
-        if j==1:
+        if j==i:
             continue
         dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1])
         prev_idx = j
