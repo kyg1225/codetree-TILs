9bd2638383c9f82dac521eccb734982c61a76e90
ygkim <daily1225@gmail.com>
2024-07-26 00:57:41 +0900

    Title: 특정 수와 근접한 합, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240726/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -5,12 +5,9 @@ n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
 mini = INT_MAX
-
+result = 0
 for i in range(n):
     for j in range(i+1, n):
-        arr[i] = 0
-        arr[j] = 0
-        result = abs(s - sum(arr))
-        mini =  min(result, mini)
-
+        result = sum(arr) - (arr[i]+arr[j])
+        mini =  min(abs(s-result), mini)
 print(mini)
